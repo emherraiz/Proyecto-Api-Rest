f9856fff1a9a44ef3a9d9bc504c1e33ac2674d87
Enrique Marina <eherraiz@myuax.com>
2022-12-07 20:15:43 +0100

    gg

--- server/films/views.py
+++ server/films/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Film, FilmGenre
+from .serializers import FilmSerializer, FilmGenreSerializer
+class FilmViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
 
-# Create your views here.
+class GenreViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = FilmGenre.objects.all()
+    serializer_class = FilmGenreSerializer
+    lookup_field = 'slug' # identificaremos los géneros usando su slug
+
