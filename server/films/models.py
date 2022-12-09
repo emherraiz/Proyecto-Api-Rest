4b7c9afbd97e1be71987cfda991ceafcb808f5b1
Enrique Marina <eherraiz@myuax.com>
2022-12-09 20:25:50 +0100

    gg

--- server/films/models.py
+++ server/films/models.py
@@ -29,6 +29,10 @@ class Film(models.Model):
 
     image_wallpaper = models.ImageField(upload_to=path_to_film, null=True, blank=True, verbose_name="Wallpaper")
 
+    favorites = models.IntegerField(default=0, verbose_name="favoritos")
+
+    average_note = models.FloatField(default=0.0, verbose_name="nota media", validators=[MaxValueValidator(10.0)])
+
     class Meta:
         verbose_name = "Película"
         ordering = ['title']
@@ -72,3 +76,27 @@ class FilmUser(models.Model):
     class Meta:
         unique_together = ['film', 'user']
         ordering = ['film__title']
+
+
+def update_film_stats(sender, instance, **kwargs):
+    # Actualizamos los favoritos contando los favoritos de esa película
+    count_favorites = FilmUser.objects.filter(
+        film=instance.film, favorite=True).count()
+    instance.film.favorites = count_favorites
+    # Actualizamos la nota recuperando el número de notas y haciendo la media
+    notes = FilmUser.objects.filter(
+        film=instance.film).exclude(note__isnull=True)
+    count_notes = notes.count()
+    sum_notes = notes.aggregate(Sum('note')).get('note__sum')
+    # Intentamos hacer la media con dos decimales usando un try
+    # Fallará si sum_notes es None como count_notes es 0
+    # Esto sucede las primeras veces porque aún no hay notas establecidas
+    try:
+        instance.film.average_note = round(sum_notes/count_notes, 2)
+    except:
+        pass
+    # Guardamos la película
+    instance.film.save()
+
+# en el post delete se pasa la copia de la instance que ya no existe
+post_save.connect(update_film_stats, sender=FilmUser)
