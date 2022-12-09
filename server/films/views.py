44ab5cc5fabc3b5e6029f05349f3d596f7ddf2ae
Enrique Marina <eherraiz@myuax.com>
2022-12-09 19:11:36 +0100

    gg

--- server/films/views.py
+++ server/films/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Film, FilmGenre
 from .serializers import FilmSerializer, FilmGenreSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 class FilmViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Film.objects.all()
     serializer_class = FilmSerializer
 
     # Sistema de filtros
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
 
-    search_fields = ['title', 'year', 'genres__name'] 
+    search_fields = ['title', 'year', 'genres__name']
+    ordering_fields = ['title', 'year', 'genres__name']  # edited
+
+    filterset_fields = {'year': ['lte', 'gte'],  'genres': ['exact']}
 
 class GenreViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = FilmGenre.objects.all()
